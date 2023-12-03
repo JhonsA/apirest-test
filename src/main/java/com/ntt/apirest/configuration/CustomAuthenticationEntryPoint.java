6620eb53571d25737194c5c253b122c793096f41
Jhons Albornoz Araya <jhons.albornoz@deepsource.cl>
2023-12-03 13:34:51 -0300

    Add error handlers

--- src/main/java/com/ntt/apirest/configuration/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/ntt/apirest/configuration/CustomAuthenticationEntryPoint.java
@@ -1,10 +1,15 @@
 package com.ntt.apirest.configuration;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,7 +21,14 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("mensaje", "Unauthorized");
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
     }
     
 }
---
+++ src/main/java/com/ntt/apirest/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ntt.apirest.configuration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ntt.apirest.domain.classes.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorResponse error = new ErrorResponse("Body invalido");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    
+}
